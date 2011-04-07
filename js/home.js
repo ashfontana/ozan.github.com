a3e505064f5a863bd09eb195d9f02d994a8787e7
Ozan Onay <ozan.onay@gmail.com>
2011-04-06 22:07:57 -0700

    Modified homepage image rendering logic to wrap photos around content, in correct order

---
+++ js/home.js
@@ -0,0 +1,94 @@
+
+String.prototype.supplant = function (o) {
+  return this.replace(/{([^{}]*)}/g, function (a, b) {
+    var r = o[b];
+    return typeof r === 'string' || typeof r === 'number' ? r : a;
+  });
+};
+
+$(function () {
+  
+  // Resize UI elements to accomodate photos nicely.
+  // For brevity, the p prefix denotes photo and the c prefix denotes container.
+  var pSize = 77; 
+  
+  // Set the container width to a multiple of the photo size wider than the doc,
+  // to use all available real estate
+  var cWidth = Math.ceil($('body').width() / pSize) * pSize;
+  $('#container').css('width', cWidth);
+  
+  // Set the content outer height to a multiple of the photo size, to avoid
+  // awkward empty space at bottom
+  var content = $('#content')
+    , prevContentOuterHeight = content.outerHeight()
+    , prevContentHeight = content.height()
+    , newContentOuterHeight = Math.ceil(prevContentOuterHeight / pSize) * pSize
+    , newContentHeight = newContentOuterHeight - (prevContentOuterHeight - prevContentHeight);
+  content.css('height', newContentHeight);
+  
+  // Set the right hand section width to occupy the last remaining space middle space.
+  var rightWidth = cWidth - content.outerWidth() - pSize * 2;
+  $('#container .right').css('width', rightWidth);
+  
+  var populateTop = function (photos) {
+    // Fill the top section with as many photos as possible, and return the remainder
+    var target = $('#container .top');
+    var slots = (cWidth / pSize) * 2;
+    for (var i = 0; i < slots; i++) {
+      target.append(photos.pop());
+    }
+    return photos;
+  };
+  
+  var populateMiddle = function (photos) {
+    // Fill the left and right sections with as many photos as possible, maintaining
+    // order such that it appears that photos are ordered left-to-right despite being
+    // in different divs.
+    var left = $('#container .left')
+      , right = $('#container .right')
+      , content = $('#content')
+      , hSlots = content.outerHeight() / pSize
+      , rSlots = (cWidth - content.outerWidth() - pSize * 2) / pSize;
+      
+    for (var h = 0; h < hSlots; h++) {
+      for (var l = 0; l < 2; l ++) {
+        left.append(photos.pop());
+      }
+      for (var r = 0; r < rSlots; r++) {
+        right.append(photos.pop());
+      }
+    }
+    return photos;
+  }
+    
+  var imgTemplate = "<img src='http://farm{farm}.static.flickr.com/{server}/{id}_{secret}_s.jpg' />";
+  
+  $.getJSON("http://api.flickr.com/services/rest/",
+      {
+        method: 'flickr.photosets.getPhotos',
+        api_key: '7d7dbfe46562af3c2cd3080fcedcf01c',
+        photoset_id: '72157626022484506',
+        format: 'json',
+        nojsoncallback: 1
+      },
+      function (data) {
+        if (data.stat === 'ok') {
+          var photos = $.map(data.photoset.photo, function (photo, i) {
+            var img = $(imgTemplate.supplant(photo));
+            // img.css('display', 'none').bind('load', function () {
+            //   $(this).fadeIn(Math.random() * 5000);
+            // });
+            return img;
+          });
+          
+          photos = populateTop(photos)
+          photos = populateMiddle(photos)
+          
+          $.each(photos, function (i, photo) {
+            $('#container .bottom').append(photo);
+          })
+        }
+      });
+      
+});
+
